8ff115d48736b7d05dd47386aeed15698b088c5a
Vladislav Samoilenko <vlad.samoilenko.it@gmail.com>
2022-12-24 22:15:24 +0200

    UI: Added extending Button props from HTMLButtonElement

--- packages/ui/src/components/Button/types/index.ts
+++ packages/ui/src/components/Button/types/index.ts
@@ -1,10 +1,12 @@
+import React from "react";
+
 export enum ButtonSize {
   Normal = "normal",
   Large = "large",
 }
 
-export interface ButtonProps {
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "outlined" | "contained" | "text" | "link";
   size?: "small" | "normal" | "large";
-  disabled?: boolean;
 }

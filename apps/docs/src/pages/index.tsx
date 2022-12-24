c3242bed883a01865056e88206ee858092eb3e4e
Vladislav Samoilenko <vlad.samoilenko.it@gmail.com>
2022-12-24 12:44:50 +0200

    Setting nextjs with styled-components

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,11 +1,17 @@
-import { H1, H2 } from "@hs-job/ui";
+import { H1 } from "@hs-job/ui";
 import React from "react";
+import styled from "styled-components";
+
+const MyText = styled.h1`
+  color: red;
+  font-size: 50px;
+`;
 
 export default function Docs() {
   return (
     <div>
-      <H1>Docs12</H1>
-      <H2>Docs22</H2>
+      <MyText>Docs [1]</MyText>
+      <H1>Docs [2]</H1>
     </div>
   );
 }

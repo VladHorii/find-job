5f6d9ba97d9d4b3c4984564e2f3a03f77da66bee
Vladislav Samoilenko <vlad.samoilenko.it@gmail.com>
2022-12-25 00:05:05 +0200

    Added rules for ESLint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,9 +1,27 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier", "eslint:recommended"],
+  extends: [
+    "next",
+    "turbo",
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
     quotes: ["warn", "double"],
-    "no-unused-vars": "warn",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/ban-types": "error",
+    "@typescript-eslint/no-empty-interface": "error",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-extra-non-null-assertion": "error",
+    "@typescript-eslint/array-type": [
+      "warn",
+      { default: "array", readonly: "array" },
+    ],
   },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
 };

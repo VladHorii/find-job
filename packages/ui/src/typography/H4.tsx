24b5e3be17fa5ff80a5b45f9137728841b92d803
LaRossa63 <maksimsevostuanow@mail.ru>
2022-12-27 14:24:23 +0400

    Updated typography

--- packages/ui/src/typography/H4.tsx
+++ packages/ui/src/typography/H4.tsx
@@ -2,24 +2,21 @@ import { MediaQuery } from "@hs-job/types";
 import styled from "styled-components";
 
 export const H4 = styled.h4`
+  line-height: 150%;
+  font-size: 15px;
   font-family: ${(props) => props.theme.typography.fontFamily};
   font-style: ${(props) => props.theme.typography.fontStyle};
   font-weight: ${(props) => props.theme.typography.fontWeight.normal};
 
   color: ${(props) => props.theme.palette.common.black};
 
-  @media (min-width: ${MediaQuery.Desktop}px) {
-    line-height: 120%;
-    font-size: 24px;
-  }
-
   @media (min-width: ${MediaQuery.Tablet}px) {
     line-height: 150%;
     font-size: 18px;
   }
 
-  @media (min-width: ${MediaQuery.Mobile}px) {
-    line-height: 150%;
-    font-size: 15px;
+  @media (min-width: ${MediaQuery.Desktop}px) {
+    line-height: 120%;
+    font-size: 24px;
   }
 `;
--- packages/ui/src/typography/H5.tsx
+++ packages/ui/src/typography/H5.tsx
@@ -2,24 +2,21 @@ import { MediaQuery } from "@hs-job/types";
 import styled from "styled-components";
 
 export const H5 = styled.h5`
+  line-height: 155%;
+  font-size: 12px;
   font-family: ${(props) => props.theme.typography.fontFamily};
   font-style: ${(props) => props.theme.typography.fontStyle};
   font-weight: ${(props) => props.theme.typography.fontWeight.normal};
 
   color: ${(props) => props.theme.palette.common.black};
 
-  @media (min-width: ${MediaQuery.Desktop}px) {
-    line-height: 130%;
-    font-size: 16px;
-  }
-
   @media (min-width: ${MediaQuery.Tablet}px) {
     line-height: 155%;
     font-size: 12px;
   }
 
-  @media (min-width: ${MediaQuery.Mobile}px) {
-    line-height: 155%;
-    font-size: 12px;
+  @media (min-width: ${MediaQuery.Desktop}px) {
+    line-height: 130%;
+    font-size: 16px;
   }
 `;

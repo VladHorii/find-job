247cb292dad6ed8fa3d5421585cf92c907a9669c
LaRossa63 <maksimsevostuanow@mail.ru>
2022-12-28 22:08:42 +0400

    UI/Input: updated input

--- packages/ui/src/components/Input/components/Input.tsx
+++ packages/ui/src/components/Input/components/Input.tsx
@@ -1,55 +1,96 @@
+import { IconButton, Input as MuiInput } from "@mui/material";
 import { FC } from "react";
 import styled, { css } from "styled-components";
 import { InputProps, InputSize } from "../types";
 
-export const Input: FC<InputProps> = ({ size, ...props }) => {
-  return <MyInput inputSize={size} {...props} />;
-};
-
-interface MyInputProps {
-  inputSize: InputProps["size"];
+export interface InputStyledProps {
+  mysize: InputProps["size"];
 }
 
-const MyInput = styled.input<MyInputProps>`
-  font-family: ${(props) => props.theme.typography.fontFamily};
-  font-style: ${(props) => props.theme.typography.fontStyle};
-  font-weight: ${(props) => props.theme.typography.fontWeight.small};
-  font-size: 14px;
-  line-height: 17px;
-  text-align: start;
-  color: ${(props) => props.theme.palette.common.black};
+export const Input: FC<InputProps> = ({ size, ...props }) => {
+  const { icon, position, handleClickIcon } = props;
 
-  padding: 5px 10px;
+  if (icon) {
+    if (position === "end") {
+      return (
+        <InputStyled
+          mysize={size}
+          {...props}
+          startAdornment={
+            <IconButton onClick={handleClickIcon}>{icon}</IconButton>
+          }
+        />
+      );
+    }
 
-  border: 1px solid ${(props) => props.theme.palette.secondary.active};
-  border-radius: 8px;
-
-  ${(props) =>
-    props.inputSize === InputSize.Normal &&
-    css`
-      padding: 5px 10px;
-
-      font-weight: ${(props) => props.theme.typography.fontWeight.normal};
-      font-size: 16px;
-      line-height: 19px;
-    `}
-
-  ${(props) =>
-    props.inputSize === InputSize.Large &&
-    css`
-      padding: 5px 10px;
-
-      font-weight: ${(props) => props.theme.typography.fontWeight.large};
-      font-size: 18px;
-      line-height: 22px;
-    `}
-    
-  &:hover {
-    border: 1px solid ${(props) => props.theme.palette.actions.hover};
+    return (
+      <InputStyled
+        mysize={size}
+        {...props}
+        endAdornment={<IconButton onClick={handleClickIcon}>{icon}</IconButton>}
+      />
+    );
   }
 
-  &:disabled {
-    color: ${(props) => props.theme.palette.actions.disabled};
-    border: 1px solid ${(props) => props.theme.palette.actions.disabled};
+  return <InputStyled mysize={size} {...props} />;
+};
+
+const InputStyled = styled(MuiInput)<InputStyledProps>`
+  &.MuiInputBase-root {
+    width: fit-content;
+
+    font-family: ${(props) => props.theme.typography.fontFamily};
+    font-style: ${(props) => props.theme.typography.fontStyle};
+    font-weight: ${(props) => props.theme.typography.fontWeight.small};
+    font-size: 14px;
+    line-height: 17px;
+    text-align: start;
+    color: ${(props) => props.theme.palette.common.black};
+
+    padding: 5px 10px;
+
+    border: 1px solid ${(props) => props.theme.palette.secondary.main};
+    border-radius: 8px;
+
+    ${(props) =>
+      props.mysize === InputSize.Normal &&
+      css`
+        padding: 5px 10px;
+
+        font-weight: ${(props) => props.theme.typography.fontWeight.normal};
+        font-size: 16px;
+        line-height: 19px;
+      `}
+
+    ${(props) =>
+      props.mysize === InputSize.Large &&
+      css`
+        padding: 5px 10px;
+
+        font-weight: ${(props) => props.theme.typography.fontWeight.large};
+        font-size: 18px;
+        line-height: 22px;
+      `}
+
+    &:hover {
+      border: 1px solid ${(props) => props.theme.palette.actions.hover};
+    }
+
+    &:disabled {
+      color: ${(props) => props.theme.palette.actions.disabled};
+      border: 1px solid ${(props) => props.theme.palette.actions.disabled};
+    }
+  }
+
+  & button {
+    padding: 0;
+  }
+
+  &:before {
+    border: none !important;
+  }
+
+  &:after {
+    border: none !important;
   }
 `;
--- packages/ui/src/components/Input/types/index.ts
+++ packages/ui/src/components/Input/types/index.ts
@@ -1,11 +1,16 @@
-import React from "react";
+import { InputProps as MuiInputProps } from "@mui/material";
 
 export enum InputSize {
   Normal = "normal",
   Large = "large",
 }
 
-export interface InputProps
-  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "size"> {
+export interface InputIcon {
+  icon?: React.ReactNode;
+  position?: "start" | "end";
+  handleClickIcon?: () => void;
+}
+
+export interface InputProps extends Omit<MuiInputProps, "size">, InputIcon {
   size?: "small" | "normal" | "large";
 }

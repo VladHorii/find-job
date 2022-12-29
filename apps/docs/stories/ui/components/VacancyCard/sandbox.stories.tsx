a8a871a0da747c5237c07aad991ddc5d9ab7cdca
Maxim Sevostyanov <maksimsevostuanow@mail.ru>
2022-12-30 00:29:03 +0400

    docs: added component VacancyCard in StoryBook

--- apps/docs/stories/ui/components/VacancyCard/sandbox.stories.tsx
+++ apps/docs/stories/ui/components/VacancyCard/sandbox.stories.tsx
@@ -1,14 +1,28 @@
 import { VacancyCard } from "@hs-job/ui";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React, { FC } from "react";
+import styled from "styled-components";
 
 export default {
-  title: "docs/ui/components/Vacancy Card",
+  title: "docs/ui/components/VacancyCard",
   component: VacancyCard,
 } as ComponentMeta<typeof VacancyCard>;
 
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Template: ComponentStory<FC> = () => {
-  return <VacancyCard />;
+  return (
+    <Container>
+      <VacancyCard />
+    </Container>
+  );
 };
 
 export const Sandbox = Template.bind({});

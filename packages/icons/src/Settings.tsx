b4cb881e4e93e54cf4b8a702602f4b243896d29f
Maxim Sevostyanov <maksimsevostuanow@mail.ru>
2023-01-14 16:41:14 +0400

    UI: updated icon Settings

--- packages/icons/src/Settings.tsx
+++ packages/icons/src/Settings.tsx
@@ -12,8 +12,8 @@ export const Settings: FC<SVGProps<SVGSVGElement>> = (props) => {
         strokeLinejoin="round"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M17 14.5C17 15.8807 15.8807 17 14.5 17C13.1193 17 12 15.8807 12 14.5C12 13.1183 13.1193 12 14.5 12C15.8807 12 17 13.1183 17 14.5Z"
         stroke="currentColor"
         strokeWidth="1.5"
@@ -29,8 +29,8 @@ export const Settings: FC<SVGProps<SVGSVGElement>> = (props) => {
         strokeLinejoin="round"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M1 3.5C1 4.88174 2.11928 6 3.5 6C4.88072 6 6 4.88174 6 3.5C6 2.11928 4.88072 1 3.5 1C2.11928 1 1 2.11928 1 3.5Z"
         stroke="currentColor"
         strokeWidth="1.5"

d5d738e1e6037b3527eb11b9d8710888d845008f
Maxim Sevostyanov <maksimsevostuanow@mail.ru>
2023-01-14 13:02:34 +0400

    updated component UI Avatar

--- packages/ui/src/components/Avatar/components/Avatar.tsx
+++ packages/ui/src/components/Avatar/components/Avatar.tsx
@@ -1,10 +1,32 @@
 import { Avatar as MuiAvatar } from "@mui/material";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { AvatarProps } from "../types";
 
-export const Avatar = styled(MuiAvatar)`
+export const Avatar = styled(MuiAvatar)<AvatarProps>`
   &.MuiAvatar-root {
     width: 50px;
     height: 50px;
-    background-color: ${(props) => props.theme.palette.success.light};
+
+    background-color: ${(props) => props.theme.palette.warning.light};
+
+    ${(props) =>
+      props.size === "normal" &&
+      css`
+        width: 150px;
+        height: 150px;
+      `}
+
+    ${(props) =>
+      props.size === "large" &&
+      css`
+        width: 200px;
+        height: 200px;
+      `}
+
+    ${(props) =>
+      props.variant === "hr" &&
+      css`
+        background-color: ${(props) => props.theme.palette.success.light};
+      `}
   }
 `;
